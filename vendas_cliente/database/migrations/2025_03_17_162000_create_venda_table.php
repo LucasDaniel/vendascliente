906c0a3112618935b4c1c130297247ea4af1a4e5
Lucas Daniel Beltrame <lucasdanielbeltrame@hotmail.com>
2025-03-17 16:26:11 -0300

    Primeiro commit com migratuion

---
+++ vendas_cliente/database/migrations/2025_03_17_162000_create_venda_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('venda', function (Blueprint $table) {
+            $table->id();
+            $table->integer('quantidade');
+            $table->unsignedBigInteger('cliente_id');
+            $table->unsignedBigInteger('produto_id');
+ 
+            $table->foreign('cliente_id')->references('id')->on('cliente');
+            $table->foreign('produto_id')->references('id')->on('produto');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('venda');
+    }
+};
